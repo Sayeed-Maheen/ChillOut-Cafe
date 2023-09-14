288e0954a3311511b0b49aa523019c617dd2c923
sayeed1124 <sayeedhassan1124@gmail.com>
2023-09-14 12:36:58 +0600

    14.09.23
    App Bar and Tab Bar added

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -8,30 +8,93 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: colorPrimary,
-      appBar: AppBar(
-        systemOverlayStyle: SystemUiOverlayStyle.dark,
-        flexibleSpace: Container(
-          color: colorSecondary, // Set a fixed color here
-        ),
-        title: Text(
-          'Good day, Sayeed',
-          style: TextStyle(
-            fontSize: 18.sp,
-            fontWeight: FontWeight.w600,
-            color: colorPrimary,
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        backgroundColor: colorPrimary,
+        appBar: AppBar(
+          systemOverlayStyle: SystemUiOverlayStyle.dark,
+          flexibleSpace: Container(
+            color: colorSecondary, // Set a fixed color here
           ),
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              Icons.notifications,
+          title: Text(
+            'Good day, Sayeed',
+            style: TextStyle(
+              fontSize: 18.sp,
+              fontWeight: FontWeight.w600,
               color: colorPrimary,
             ),
           ),
-        ],
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.notifications,
+                color: colorPrimary,
+              ),
+            ),
+          ],
+          bottom: PreferredSize(
+            preferredSize: Size.fromHeight(100.h),
+            child: Container(
+              alignment: Alignment.centerLeft,
+              // height: 100.h,
+              color: colorSecondary,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: REdgeInsets.symmetric(horizontal: 16),
+                    child: Text(
+                      'What would you like\nto drink today?',
+                      style: TextStyle(
+                        color: colorPrimary,
+                        fontSize: 15.sp,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 12.h),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                        SizedBox(width: 16.w),
+                        TabBar(
+                          isScrollable: true,
+                          indicator: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8.r),
+                              color: colorPrimary),
+                          unselectedLabelColor: colorPrimary,
+                          indicatorSize: TabBarIndicatorSize.tab,
+                          labelStyle: TextStyle(
+                              fontWeight: FontWeight.w400, fontSize: 12.sp),
+                          labelColor: colorSecondary,
+                          tabs: const [
+                            Tab(text: 'Popular'),
+                            Tab(text: 'Black coffee'),
+                            Tab(text: 'Winter special'),
+                            Tab(text: 'Cafe special'),
+                          ],
+                        ),
+                        SizedBox(width: 16.w),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 8.h),
+                ],
+              ),
+            ),
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Container(),
+            Container(),
+            Container(),
+            Container(),
+          ],
+        ),
       ),
     );
   }
